2fe214f6e54307868c194bc2536508114083ae82
hjkang94 <hjkang94@github.com>
2024-12-22 18:06:43 +0900

    Refactor: api key 이름 변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const apiKey = process.env.MOVEDB_API_KEY;
+const apiKey = process.env.REACT_APP_MOVEDB_API_KEY;
 
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
