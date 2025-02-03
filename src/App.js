abdb046fcbaf20584bb9a761e5935086ff53e95a
hjkang <hjkang94@smartscore.kr>
2025-02-03 14:55:57 +0900

    Style: 배경색 적용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+import requests from './api/requests';
 import './App.css';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from './components/Row';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav />
 
       <Banner />
+
+      <Row title="NETFLIX ORIGINALS" id="NO" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+
+      <Row title="TRENDING NOW" id="TN" fetchUrl={requests.fetchTrending} />
+
+      <Row title="TOP RATED" id="TR" fetchUrl={requests.fetchTopRated} />
+
+      <Row title="ACTION MOVIES" id="AM" fetchUrl={requests.fetchActionMovies} />
+
+      <Row title="COMEDY MOVIES" id="CM" fetchUrl={requests.fetchComedyMovies} />
     </div>
   );
 }
